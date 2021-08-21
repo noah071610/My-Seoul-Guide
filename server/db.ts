745cb99dfdad068a6552dab7bb845d918908e69f
noah <noah071610@naver.com>
2021-08-22 08:16:26 +0900

    Update : npm modules for deploy

--- server/db.ts
+++ server/db.ts
@@ -2,17 +2,6 @@ import convert from "xml-js";
 import dotenv from "dotenv";
 import fetch from "node-fetch";
 dotenv.config();
-// for (let i = 0; i < contents.item.length; i++) {
-//   const getOneOverview = await fetch(
-//     `http://api.visitkorea.or.kr/openapi/service/rest/EngService/detailCommon?ServiceKey=${process.env.TOUR_SERVICE_KEY}&contentTypeId=${typeNum}&contentId=${contents.item[i].contentid._text}&MobileOS=ETC&MobileApp=TourAPI3.0_Guide&&overviewYN=Y`
-//   )
-//     .then((res) => res.text())
-//     .then((data) => {
-//       let content = JSON.parse(convert.xml2json(data, { compact: true })).response.body.items
-//         .item;
-//       return content.overview;
-//     });
-//   contents.item[i].overview = getOneOverview;
 const getActivities = async (typeNum: number, pageNum: number) => {
   const contents = await fetch(
     `http://api.visitkorea.or.kr/openapi/service/rest/EngService/areaBasedList?ServiceKey=${process.env.TOUR_SERVICE_KEY}&contentTypeId=${typeNum}&areaCode=1&sigunguCode=&cat1=A02&cat2=&cat3=&listYN=Y&MobileOS=ETC&MobileApp=TourAPI3.0_Guide&arrange=B&numOfRows=12&pageNo=${pageNum}`
